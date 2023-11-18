c6d0cb94b8d21ee3cda37c90d955269004a1e770
85Ayan <mohammadataullah85@gmail.com>
2023-11-18 12:11:14 +0530

    initial commit

---
+++ Abc/src/Jtc.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Jtc {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		List<String> list = Arrays.asList("ram");
+		List<String> list1 = Arrays.asList("ram");
+		System.out.println(list.equals(list1));
+		
+		ArrayList list2=new ArrayList();
+		
+	}
+}
